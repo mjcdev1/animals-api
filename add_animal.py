dd4c09db61caeb7fc91cb93a905e5104c7dd5f25
mjcdev1 <mjc.developer1@gmail.com>
2024-07-22 15:52:22 +0000

    Starting database and endpoints established
    Implementing API key requirement
    
    Returning table results as json
    
    Fixing errors
    
    Updating query to be more specific
    
    Added a new parameter for the animal common name and changed to only return specific animal
    
    Trying to fix JSON item order
    
    Fixed order that JSON items appear in API result
    
    Fixing errors
    
    Fixed to show JSON dict items in correct order
    
    Adding some more argument options to filter results
    
    Refactored to use OOP principles
    
    Can now sort animals by class, order, and common name
    
    Added a better message at the index endpoint
    
    New table and column added to store animal family. Also added option to sort by families of animals
    
    Added API endpoint to add animal class, as well as a starter script to test it. Also renamed AnimalData class to DBOps (Database Operations)
    
    Refactored error logic for current endpoint
    
    Adapted get_animals endpoint to be called with a API request, and wrote a script to do so
    
    Fix
    
    Fix
    
    get_animals endpoint now adapted to be called by a requests script, and wrote the script to do so. Also refactored some code to include better error handling and provide better messages. Lastly, moved database queries into a separate file to improve readability of main app.py file
    
    TT
    
    working

---
+++ add_animal.py
@@ -0,0 +1,34 @@
+import requests
+import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv('database.env')
+
+url = 'http://127.0.0.1:5000/add_animal_data'
+url = 'https://animal-api-783131d61b3b.herokuapp.com/add_animal_data' #-- IF TESTING WITH HEROKU 
+api_key = os.getenv('API_KEY')  
+
+data = {
+    "common_name": "African bush f",
+    "class_name": "Mammalia",
+    "order_name": "Proboscidea",
+    "family_name": "Elephantidae",
+    "species_name": "Loxodonta africana",
+    "subspecies_name": None,
+    "scientific_name": "Loxodonta africanaaaaafff",
+    "conservation_status": "Endangered",
+    "attributes": None,
+    "header_img_url": None,
+    "addl_img_urls": None,
+    "species_desc": None,
+    "subspecies_desc": None,
+    "sources": None
+}
+
+response = requests.post(url, params={'api_key': api_key}, json=data)
+
+if response.status_code == 200:
+        print('Data added successfully:', response.json())
+else:
+        print('Failed to add data:', response.status_code, response.json())
---
+++ get_animals.py
@@ -0,0 +1,21 @@
+import requests
+import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv('database.env')
+
+url = 'http://127.0.0.1:5000/get_animals'
+url = 'https://animal-api-783131d61b3b.herokuapp.com/get_animals' #-- IF TESTING WITH HEROKU 
+api_key = os.getenv('API_KEY')  
+
+requested_items = {
+    "class_name": "Mammalia",
+}
+
+response = requests.get(url, params={'api_key': api_key}, json=requested_items)
+
+if response.status_code == 200:
+        print(response.json())
+else:
+        print(response.status_code, response.json())
